0b675b62de270d08be6d5aaa73e6959aa8e538c5
Tara Matthew <taramatthew.93@gmail.com>
2019-10-16 12:31:57 +0100

    Added Vuetify

--- client/app/main.js
+++ client/app/main.js
@@ -1,15 +1,19 @@
 import Vue from 'nativescript-vue'
-import App from './components/App'
+import App from './components/Register'
+import Vuetify from 'vuetify'
 
 
 import store from './store'
 
-  
+Vue.use(Vuetify)
+
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
 
 new Vue({
-  store,
-  render: h => h('frame', [h(App)])
+    vuetify: new Vuetify( {
+    }),
+    store,
+    render: h => h('frame', [h(App)])
 }).$start()
