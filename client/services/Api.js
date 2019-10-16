d67b0b9b0a76256b26009ca59d9fe7367d5e0129
Tara Matthew <taramatthew.93@gmail.com>
2019-10-16 12:25:58 +0100

    Notes for the API

--- client/services/Api.js
+++ client/services/Api.js
@@ -5,3 +5,6 @@ export default() => {
         baseURL: `http://10.0.2.2:3000`
     })
 }
+
+//Change the baseURL to computer's ip address when building for a device
+//10.0.2.2 is for the Android emulator
